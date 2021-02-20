14ecf6a763f588d87ff5e5477c22e7a5b8674aa2
chuanm <chuangmatthew@yahoo.com>
2021-02-20 18:53:47 -0500

    Add fail sfx if wrong tool used in minigame

--- Tools/BaseToolMinigame.gd
+++ Tools/BaseToolMinigame.gd
@@ -11,6 +11,7 @@ func process_input(tool_input_data: ToolInputData):
 	if tool_input_data.matches_game_type(accepted_tool_type):
 		accept_tool_input(tool_input_data)
 	else:
+		$Fail.play()
 		emit_signal("botch_made", tool_input_data.get_tool_mismatch_damage())
 
 func accept_tool_input(tool_input_data):
